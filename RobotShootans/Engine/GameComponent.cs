ca87cff0550d3bb5a7ab5bff0202ed8a4b841b10
BunnyRabbit2 (Lewis Storey) <iamlewisstorey@yahoo.com>
2014-03-03 21:12:40 +0000

    Finished porting Engine code over from IvoryHammer
    Signed-off-by: BunnyRabbit2 (Lewis Storey) <iamlewisstorey@yahoo.com>

--- RobotShootans/Engine/GameComponent.cs
+++ RobotShootans/Engine/GameComponent.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +9,37 @@ namespace RobotShootans.Engine
 {
     public class GameComponent
     {
+        protected bool _loaded;
+        public bool Loaded { get { return _loaded; } }
+
+        string _componentName;
+        public string ComponentName { get { return _componentName; } }
+
+        public GameScreen GameScreen;
+
+        public GameComponent(string componentName)
+        {
+            _componentName = componentName;
+        }
+
+        public void Load()
+        {
+
+        }
+
+        public void Unload()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch sBatch)
+        {
+
+        }
     }
 }
--- RobotShootans/Engine/GameScreen.cs
+++ RobotShootans/Engine/GameScreen.cs
@@ -11,25 +11,26 @@ namespace RobotShootans.Engine
     {
         public GameEngine Engine;
 
-        private HashSet<GameComponent> _gameComponents;
-        private bool _loaded;
+        protected HashSet<GameComponent> _gameComponents;
+        protected bool _loaded;
         public bool Loaded { get { return _loaded; } }
 
-        private bool _paused;
+        protected bool _paused;
         public bool Paused { get { return _paused; } }
         public void Pause() { _paused = true; }
         public void Unpause() { _paused = false; }
 
-        private bool _blockUpdating;
+        protected bool _blockUpdating;
         public bool BlockUpdating { get { return _blockUpdating; } }
 
-        private string _screenName;
+        protected string _screenName;
         public string ScreenName { get { return _screenName; } }
 
-        private HashSet<GameComponent> _componentsToRemove;
+        protected HashSet<GameComponent> _componentsToRemove;
         
         public GameScreen(bool blockUpdating = false)
         {
+            _gameComponents = new HashSet<GameComponent>();
             _componentsToRemove = new HashSet<GameComponent>();
         }
 
@@ -45,22 +46,44 @@ namespace RobotShootans.Engine
 
         public void Update(GameTime gameTime)
         {
-
+            if(!_paused)
+            {
+                foreach(GameComponent gc in _gameComponents)
+                {
+                    gc.Update(gameTime);
+                }
+            }
         }
 
         public void Draw(GameTime gameTime, SpriteBatch sBatch)
         {
-
+            foreach (GameComponent gc in _gameComponents)
+            {
+                gc.Draw(gameTime, sBatch);
+            }
         }
 
-        public void addComponent(GameComponent componentIn, string nameIn = "")
+        public void addComponent(GameComponent componentIn)
         {
-
+            componentIn.GameScreen = this;
+            componentIn.Load();
+            _gameComponents.Add(componentIn);
         }
 
         public void removeComponent(GameComponent componentIn)
         {
+            _componentsToRemove.Add(componentIn);
+        }
 
+        protected void removeComponenets()
+        {
+            if(_componentsToRemove.Count > 0)
+            {
+                foreach(GameComponent gc in _componentsToRemove)
+                {
+                    _gameComponents.Remove(gc);
+                }
+            }
         }
     }
 }
