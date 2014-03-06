65da250c26069c120901f1a203d0502d8bc29118
BunnyRabbit2 (Lewis Storey) <iamlewisstorey@yahoo.com>
2014-03-06 16:56:45 +0000

    Changed SplashScreen and SplashLogo to have changable display times No more magic numbers! :D
    Signed-off-by: BunnyRabbit2 (Lewis Storey) <iamlewisstorey@yahoo.com>

--- RobotShootans/Entities/SplashLogo.cs
+++ RobotShootans/Entities/SplashLogo.cs
@@ -8,6 +8,9 @@ using Microsoft.Xna.Framework;
 
 namespace RobotShootans.Entities
 {
+    /// <summary>
+    /// A splash logo that will fade in, display for some time and then fade out.
+    /// </summary>
     public class SplashLogo : GameEntity
     {
         string _logoLocation;
@@ -16,8 +19,13 @@ namespace RobotShootans.Entities
 
         private int _fadeTimer;
         private float _fadeAlpha;
+        private int _fadeTime;
+        private int _displayTime;
 
         private Vector2 _position;
+        /// <summary>
+        /// The position of the Logo
+        /// </summary>
         public Vector2 Position
         {
             get { return _position; }
@@ -27,22 +35,57 @@ namespace RobotShootans.Entities
 
         private bool _firstUpdate;
 
+        /// <summary>
+        /// Creates the logo
+        /// </summary>
+        /// <param name="logoLocation">Location of the Splash image</param>
+        /// <param name="logoPosition">Position of the Splash logo</param>
+        /// <param name="logoColour">Colour of the Splash Logo</param>
         public SplashLogo(string logoLocation, Vector2 logoPosition, Color logoColour)
             : base(logoLocation)
         {
             _logoLocation = logoLocation;
             _position = logoPosition;
             _colour = logoColour;
+            _fadeTime = 1000;
+            _displayTime = 5000;
         }
 
+        /// <summary>
+        /// Creates the logo
+        /// </summary>
+        /// <param name="logoLocation">Location of the Splash image</param>
+        /// <param name="logoPosition">Position of the Splash logo</param>
         public SplashLogo(string logoLocation, Vector2 logoPosition)
             : base(logoLocation)
         {
             _logoLocation = logoLocation;
             _position = logoPosition;
             _colour = Color.White;
+            _fadeTime = 1000;
+            _displayTime = 5000;
         }
 
+        /// <summary>
+        /// Creates the logo
+        /// </summary>
+        /// <param name="logoLocation">Location of the Splash image</param>
+        /// <param name="logoPosition">Position of the Splash logo</param>
+        /// <param name="displayTime">How long to display the Splash logo in milliseconds</param>
+        /// <param name="fadeTime">How long the fade in and out takes in milliseconds</param>
+        public SplashLogo(string logoLocation, Vector2 logoPosition, int displayTime, int fadeTime)
+            : base(logoLocation)
+        {
+            _logoLocation = logoLocation;
+            _position = logoPosition;
+            _colour = Color.White;
+            _fadeTime = fadeTime;
+            _displayTime = displayTime;
+        }
+
+        /// <summary>
+        /// Loads the logo content and sets thew origin
+        /// </summary>
         public override void Load()
         {
             _image = Screen.Engine.Content.Load<Texture2D>(_logoLocation);
@@ -53,6 +96,10 @@ namespace RobotShootans.Entities
             _loaded = true;
         }
 
+        /// <summary>
+        /// Sets up the fading in and out
+        /// </summary>
+        /// <param name="gameTime"></param>
         public override void Update(GameTime gameTime)
         {
             if (_firstUpdate)
@@ -63,20 +110,25 @@ namespace RobotShootans.Entities
 
             _fadeTimer += gameTime.ElapsedGameTime.Milliseconds;
 
-            if(_fadeTimer < 1000)
+            if(_fadeTimer < _fadeTime)
             {
                 _fadeAlpha = _fadeTimer / 1000f;
             }
-            else if(_fadeTimer > 1000 && _fadeTimer < 4000)
+            else if(_fadeTimer > _fadeTime && _fadeTimer < (_displayTime - _fadeTime) )
             {
                 _fadeAlpha = 1.0f;
             }
-            else if (_fadeTimer > 4000)
+            else if (_fadeTimer > (_displayTime - _fadeTime))
             {
-                _fadeAlpha = (5000f - _fadeTimer) / 1000f;
+                _fadeAlpha = (_displayTime - _fadeTimer) / 1000f;
             }
         }
 
+        /// <summary>
+        /// Draws the logo
+        /// </summary>
+        /// <param name="gameTime"></param>
+        /// <param name="sbatch"></param>
         public override void Draw(GameTime gameTime, SpriteBatch sbatch)
         {
             sbatch.Draw(_image, position: _position, origin: _origin, color: _colour * _fadeAlpha);
--- RobotShootans/Screens/SplashScreen.cs
+++ RobotShootans/Screens/SplashScreen.cs
@@ -9,11 +9,20 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace RobotShootans.Screens
 {
+    /// <summary>
+    /// Displays the Splash Logos. Best to use as the first screen shown.
+    /// </summary>
     public class SplashScreen : GameScreen
     {
         private int _splashTimer;
+        private int _displayTime; // How long to display each logo for in milliseconds
+        private int _fadeTime; // How long to fade each logo in/out in milliseconds
+        private int _waitTime; // Time to wait before displaying logos
         private bool _firstUpdate, _addedFirstScreen, _addedSecondScreen;
 
+        /// <summary>
+        /// Loads the background and sets variables.
+        /// </summary>
         public override void loadGameScreen()
         {
             addEntity(new ColouredRectangle(new Rectangle(0, 0, 1920, 1080), Color.DarkGray));
@@ -23,14 +32,25 @@ namespace RobotShootans.Screens
             _addedFirstScreen = false;
             _addedSecondScreen = false;
 
+            _displayTime = 5000;
+            _fadeTime = 1000;
+            _waitTime = 2000;
+
             _loaded = true;
         }
 
+        /// <summary>
+        /// Unloads non-managed content
+        /// </summary>
         public override void unloadGameScreen()
         {
             
         }
 
+        /// <summary>
+        /// Sorts out displaying the logos properly
+        /// </summary>
+        /// <param name="gameTime"></param>
         public override void Update(GameTime gameTime)
         {
             if (_firstUpdate)
@@ -41,18 +61,19 @@ namespace RobotShootans.Screens
 
             _splashTimer += gameTime.ElapsedGameTime.Milliseconds;
 
-            if(_splashTimer > 1000 && !_addedFirstScreen)
+            // Waits two seconds before doing anything because reasons.
+            if(_splashTimer > _waitTime && !_addedFirstScreen)
             {
-                addEntity(new SplashLogo("images/funkyrabbit logo", new Vector2(Engine.RenderWidth / 2, Engine.RenderHeight / 2)));
+                addEntity(new SplashLogo("images/funkyrabbit logo", new Vector2(Engine.RenderWidth / 2, Engine.RenderHeight / 2), _displayTime, _fadeTime));
                 _addedFirstScreen = true;
             }
-            else if(_splashTimer > 6000 && !_addedSecondScreen)
+            else if(_splashTimer > _waitTime + _displayTime && !_addedSecondScreen)
             {
                 removeEntity("images/funkyrabbit logo");
-                addEntity(new SplashLogo("images/MonogameLogo512x512", new Vector2(Engine.RenderWidth / 2, Engine.RenderHeight / 2)));
+                addEntity(new SplashLogo("images/MonogameLogo512x512", new Vector2(Engine.RenderWidth / 2, Engine.RenderHeight / 2), _displayTime, _fadeTime));
                 _addedSecondScreen = true;
             }
-            else if(_splashTimer > 11000)
+            else if(_splashTimer > _waitTime + (_displayTime*2))
             {
                 Engine.removeGameScreen(this);
             }
