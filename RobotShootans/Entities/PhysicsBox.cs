9585d0f923a0fafa3948e72844c38005bbb3c9f4
Lewis Storey <iamlewisstorey@yahoo.com>
2014-06-19 16:27:56 +0100

    Created PhysicsBox Entity Collects a ColouredRectangle and a Farseer Rectangle Probably need some more code later for access to stuff in it
    Signed-off-by: Lewis Storey <iamlewisstorey@yahoo.com>

---
+++ RobotShootans/Entities/PhysicsBox.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RobotShootans.Engine;
+using FarseerPhysics.Dynamics;
+using Microsoft.Xna.Framework;
+using FarseerPhysics.Factories;
+using FarseerPhysics;
+
+namespace RobotShootans.Entities
+{
+    /// <summary>
+    /// A quick little entity to make creating visible physics boxes easier
+    /// </summary>
+    public class PhysicsBox : GameEntity
+    {
+        Body _physicsBody;
+        ColouredRectangle _displayRect;
+
+        public Vector2 SimPos { get { return _physicsBody.Position; } }
+        public Vector2 Position { get { return new Vector2(_displayRect.X, _displayRect.Y); } }
+
+        /// <summary>
+        /// Creates the physics box
+        /// </summary>
+        public PhysicsBox()
+            : base ("PHYSICS_BOX")
+        {
+            
+        }
+
+        public override void Load()
+        {
+            
+        }
+
+        public void SetupBox(World worldIn, int width, int height, bool isStaticIn, Vector2 position, float rotationIn, float restitution, float friction, Color colorIn, OriginPosition originIn = OriginPosition.CENTER)
+        {
+            if (Screen != null)
+            {
+                _physicsBody = BodyFactory.CreateRectangle(worldIn, ConvertUnits.ToSimUnits(width), ConvertUnits.ToSimUnits(height), 10f);
+                _physicsBody.Restitution = restitution;
+                _physicsBody.Friction = friction;
+                _physicsBody.Position = ConvertUnits.ToSimUnits(position);
+                _physicsBody.Rotation = MathHelper.ToRadians(rotationIn);
+                _physicsBody.IsStatic = isStaticIn;
+                _displayRect = new ColouredRectangle(new Rectangle((int)position.X, (int)position.Y, width, height), colorIn, originIn);
+
+                Screen.addEntity(_displayRect);
+
+                _loaded = true;
+            }
+            else
+            {
+                LogFile.LogStringLine("Failed to set up physics box due to no having screen set", LogType.ERROR);
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            _displayRect.Position = ConvertUnits.ToDisplayUnits(_physicsBody.Position);
+            _displayRect.setRotation(_physicsBody.Rotation);
+        }
+
+        public void Dispose()
+        {
+            if (_physicsBody != null)
+                _physicsBody.Dispose();
+        }
+    }
+}
--- RobotShootans/Screens/PhysicsTestScreen.cs
+++ RobotShootans/Screens/PhysicsTestScreen.cs
@@ -17,12 +17,9 @@ namespace RobotShootans.Screens
     /// </summary>
     public class PhysicsTestScreen : GameScreen
     {
-        Body _ground;
-        Body _platform1, _platform2;
-        List<ColouredRectangle> _displayBoxes;
-        List<Body> _boxes;
+        List<PhysicsBox> _boxes;
 
-        ColouredRectangle _groundDisp, _plat1Disp, _plat2Disp;
+        PhysicsBox _groundBox, _platform1, _platform2;
 
         private World _physicsWorld;
         /// <summary>
@@ -47,49 +44,21 @@ namespace RobotShootans.Screens
         {
             _physicsWorld = new World(new Vector2(0f, 10f));
 
-            _boxes = new List<Body>();
-            _displayBoxes = new List<ColouredRectangle>();
+            _boxes = new List<PhysicsBox>();
 
             addEntity(new ColouredRectangle(new Rectangle(0, 0, 1920, 1080), Color.DarkGray));
 
-            _ground = BodyFactory.CreateRectangle(_physicsWorld, ConvertUnits.ToSimUnits(Engine.RenderWidth * 2), ConvertUnits.ToSimUnits(50), 10f);
-            _ground.Position = ConvertUnits.ToSimUnits(Engine.RenderOrigin.X, Engine.RenderHeight-50);
-            _ground.IsStatic = true;
-            _ground.Restitution = 0.2f;
-            _ground.Friction = 0.2f;
-            _groundDisp = new ColouredRectangle(new Rectangle(0, 0, Engine.RenderWidth, 50), Color.Yellow);
-            _groundDisp.setOrigin(OriginPosition.CENTER);
+            _groundBox = new PhysicsBox();
+            addEntity(_groundBox);
+            _groundBox.SetupBox(_physicsWorld, Engine.RenderWidth * 2, 50, true, new Vector2(Engine.RenderOrigin.X, Engine.RenderHeight - 50), 0.0f, 0.2f, 0.2f, Color.Yellow, OriginPosition.CENTER);
 
-            addEntity(_groundDisp);
+            _platform1 = new PhysicsBox();
+            addEntity(_platform1);
+            _platform1.SetupBox(_physicsWorld, 500, 50, true, new Vector2(Engine.RenderWidth * 0.33f, Engine.RenderHeight * 0.5f), 45f, 0.2f, 0.2f, Color.Yellow);
 
-            _platform1 = BodyFactory.CreateRectangle(_physicsWorld, ConvertUnits.ToSimUnits(500), ConvertUnits.ToSimUnits(50), 10f);
-            _platform1.Position = ConvertUnits.ToSimUnits(Engine.RenderWidth * 0.33, Engine.RenderHeight *0.5);
-            _platform1.IsStatic = true;
-            _platform1.Restitution = 0.2f;
-            _platform1.Friction = 0.2f;
-            _platform1.Rotation = MathHelper.ToRadians(45f);
-            _plat1Disp = new ColouredRectangle(new Rectangle(0, 0, 500, 50), Color.Yellow);
-            _plat1Disp.setOrigin(OriginPosition.CENTER);
-
-            addEntity(_plat1Disp);
-
-            _platform2 = BodyFactory.CreateRectangle(_physicsWorld, ConvertUnits.ToSimUnits(500), ConvertUnits.ToSimUnits(50), 10f);
-            _platform2.Position = ConvertUnits.ToSimUnits(Engine.RenderWidth * 0.66, Engine.RenderHeight * 0.5);
-            _platform2.IsStatic = true;
-            _platform2.Restitution = 0.2f;
-            _platform2.Friction = 0.2f;
-            _platform2.Rotation = MathHelper.ToRadians(-45f);
-            _plat2Disp = new ColouredRectangle(new Rectangle(0, 0, 500, 50), Color.Yellow);
-            _plat2Disp.setOrigin(OriginPosition.CENTER);
-
-            addEntity(_plat2Disp);
-
-            _groundDisp.Position = ConvertUnits.ToDisplayUnits(_ground.Position);
-            _groundDisp.setRotation(_ground.Rotation);
-            _plat1Disp.Position = ConvertUnits.ToDisplayUnits(_platform1.Position);
-            _plat1Disp.setRotation(_platform1.Rotation);
-            _plat2Disp.Position = ConvertUnits.ToDisplayUnits(_platform2.Position);
-            _plat2Disp.setRotation(_platform2.Rotation);
+            _platform2 = new PhysicsBox();
+            addEntity(_platform2);
+            _platform2.SetupBox(_physicsWorld, 500, 50, true, new Vector2(Engine.RenderWidth * 0.66f, Engine.RenderHeight * 0.5f), -45f, 0.2f, 0.2f, Color.Yellow);
 
             _loaded = true;
         }
@@ -117,51 +86,25 @@ namespace RobotShootans.Screens
                 width = 50;
                 height = 50;
 
-                Body body = BodyFactory.CreateRectangle(_physicsWorld,
-                    ConvertUnits.ToSimUnits(width),
-                    ConvertUnits.ToSimUnits(height),
-                    10f,
-                    new Point(width, height));
+                PhysicsBox pBox = new PhysicsBox();
+                addEntity(pBox);
 
-                body.BodyType = BodyType.Dynamic;
-                body.Restitution = 0.2f;
-                body.Friction = 0.2f;
+                pBox.SetupBox(_physicsWorld, width, height, false, new Vector2(random.Next((int)(Engine.RenderWidth * 0.25), (int)(Engine.RenderWidth * 0.75)), 0), 0f, 0.2f, 0.2f, Color.Red);
 
-                body.Position = ConvertUnits.ToSimUnits(random.Next((int)(Engine.RenderWidth * 0.25), (int)(Engine.RenderWidth * 0.75)), 0);
-
-                _boxes.Add(body);
-                ColouredRectangle dispRect = 
-                    new ColouredRectangle(
-                        new Rectangle(
-                                (int)ConvertUnits.ToDisplayUnits(body.Position.X),
-                                (int)ConvertUnits.ToDisplayUnits(body.Position.Y),
-                                width,
-                                height),
-                            Color.Red);
-                dispRect.setOrigin(OriginPosition.CENTER);
-                addEntity(dispRect);
-
-                _displayBoxes.Add(dispRect);
+                _boxes.Add(pBox);
             }
 
-            for (int i = 0; i < _displayBoxes.Count; i++)
+            for (int i = 0; i < _boxes.Count; i++)
             {
-                Vector2 boxP = ConvertUnits.ToDisplayUnits(_boxes[i].Position);
+                Vector2 boxP = _boxes[i].Position;
 
                 if (boxP.X < 0 || boxP.X > Engine.RenderWidth
                     || boxP.Y > Engine.RenderHeight)
                 {
-                    Body b = _boxes[i];
+                    PhysicsBox b = _boxes[i];
                     _boxes.Remove(_boxes[i]);
                     b.Dispose();
-                    removeEntity(_displayBoxes[i]);
-                    _displayBoxes.Remove(_displayBoxes[i]);
-                }
-                else
-                {
-                    _displayBoxes[i].Position = boxP;
-                    //_displayBoxes[i].Position = ConvertUnits.ToDisplayUnits(_boxes[i].Position);
-                    _displayBoxes[i].setRotation(_boxes[i].Rotation);
+                    removeEntity(_boxes[i]);
                 }
             }
 
