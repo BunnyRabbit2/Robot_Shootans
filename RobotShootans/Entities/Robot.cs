d0a6446f4a0108b38054425124368cdf4dd0403e
Lewis Storey <iamlewisstorey@yahoo.com>
2014-07-02 13:36:31 +0100

    Robots fucking work. Holy shit. They fucking work. Too much god damn fucking around to get that done. Turns out I was being a twat and forgetting where to use radians vs degrees What a bellend.
    Signed-off-by: Lewis Storey <iamlewisstorey@yahoo.com>

--- RobotShootans/Entities/Robot.cs
+++ RobotShootans/Entities/Robot.cs
@@ -1,4 +1,6 @@
-﻿using FarseerPhysics.Dynamics;
+﻿using FarseerPhysics;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
 using Microsoft.Xna.Framework;
 using RobotShootans.Engine;
 using System;
@@ -9,9 +11,8 @@ namespace RobotShootans.Entities
     public class Robot : GameEntity
     {
         ColouredRectangle _displayRect;
-        PhysicsBox _displayBox;
+        Body _physicsBody;
         RobotSpawner _spawner;
-        float _speed;
 
         Vector2 _startPos;
 
@@ -28,26 +29,32 @@ namespace RobotShootans.Entities
             _robotSize = sizeIn;
 
             _startPos = positionIn;
-
-            _displayBox = new PhysicsBox();
-            _displayRect = new ColouredRectangle(new Rectangle((int)positionIn.X, (int)positionIn.Y, _robotSize, _robotSize), Color.Red);
-            _displayRect.setOrigin(OriginPosition.CENTER);
         }
 
         /// <summary></summary>
         public override void Load()
         {
-            // Screen.addEntity(_displayRect);
-            Screen.addEntity(_displayBox);
-            _speed = Screen.Engine.RenderHeight / 4.0f;
+            if (!Screen.PhysicsEnabled)
+            {
+                LogFile.LogStringLine("FAILED TO LOAD PLAYER. SCREEN OWNER IS NOT PHYSICS ENABLED", LogType.ERROR);
+                return;
+            }
 
-            _displayBox.SetupBox(Screen.PhysicsWorld, _robotSize, _robotSize, false, _startPos, 0.0f, 0.0f, 0.2f, Color.Red);
-            // _displayBox.Body.FixedRotation = true;
+            _physicsBody = BodyFactory.CreateRectangle(Screen.PhysicsWorld, ConvertUnits.ToSimUnits(_robotSize), ConvertUnits.ToSimUnits(_robotSize), 10f);
+            _physicsBody.Restitution = 0f;
+            _physicsBody.Friction = 0.2f;
+            _physicsBody.Position = ConvertUnits.ToSimUnits(_startPos);
+            _physicsBody.IsStatic = false;
+            _physicsBody.FixedRotation = true;
+
+            _displayRect = new ColouredRectangle(new Rectangle((int)_startPos.X, (int)_startPos.Y, _robotSize, _robotSize), Color.Red);
+            _displayRect.setOrigin(OriginPosition.CENTER);
+            Screen.addEntity(_displayRect);
 
             _maxVelocity = 3f; // Max velocity at which the robot can move
             _maxAngleVelocity = (float)Math.Sqrt(((double)_maxVelocity * (double)_maxVelocity) / 2.0);
 
-            _moveImpulse = 3f;
+            _moveImpulse = 10f;
             _angleMoveImpulse = (float)Math.Sqrt(((double)_moveImpulse * (double)_moveImpulse) / 2.0);
 
             _loaded = true;
@@ -57,18 +64,21 @@ namespace RobotShootans.Entities
         /// <param name="gameTime"></param>
         public override void Update(GameTime gameTime)
         {
-            Vector2 pos1 = _displayBox.Position;
+            Vector2 pos1 = _displayRect.Position;
             Vector2 pos2 = _spawner.playerPosition;
 
             if (pos2 != null)
             {
-                float _travelDirection = HelperFunctions.GetBearingBetweenTwoPoints(pos1, pos2);
-                _displayBox.setRotation(_travelDirection);
+                float _travelDirection = HelperFunctions.GetBearingBetweenTwoPoints(pos1, pos2, false);
+                _physicsBody.Rotation = _travelDirection;
+                _displayRect.setRotation(_travelDirection);
+
+                _travelDirection = MathHelper.ToDegrees(_travelDirection);
 
                 Vector2 impulse = Vector2.Zero;
 
-                double h = Math.Sqrt(_maxVelocity * _maxVelocity + _maxVelocity * _maxVelocity);
-                // double h = Math.Sqrt(_angleMoveImpulse * _angleMoveImpulse + _angleMoveImpulse * _angleMoveImpulse);
+                // double h = Math.Sqrt(_maxVelocity * _maxVelocity + _maxVelocity * _maxVelocity);
+                double h = Math.Sqrt(_angleMoveImpulse * _angleMoveImpulse + _angleMoveImpulse * _angleMoveImpulse);
 
                 double theta = 0;
 
@@ -79,53 +89,39 @@ namespace RobotShootans.Entities
                 {
                     theta = 90 - _travelDirection;
                     hori = 1;
-                    vert = 1;
+                    vert = -1;
                 }
                 else if (_travelDirection >= 90 || _travelDirection < 180)
                 {
                     theta = _travelDirection - 90;
                     hori = 1;
-                    vert = -1;
+                    vert = 1;
                 }
                 else if (_travelDirection >= 180 || _travelDirection < 270)
                 {
                     theta = 270 - _travelDirection;
                     hori = -1;
-                    vert = -1;
+                    vert = 1;
                 }
                 else if (_travelDirection >= 270 || _travelDirection < 360)
                 {
                     theta = _travelDirection - 270;
                     hori = -1;
-                    vert = 1;
+                    vert = -1;
                 }
+
+                theta = MathHelper.ToRadians((float)theta);
 
                 float vertI = (float)(Math.Sin(theta) * h) * vert;
                 float horiI = (float)(Math.Cos(theta) * h) * hori;
 
-                _displayBox.Body.LinearVelocity = new Vector2(horiI, vertI);
-                //_displayBox.Body.ApplyLinearImpulse(new Vector2(horiI, vertI), _displayBox.SimPos);
-
-                // soh cah
-
-                // fucking trig always fucking with my shit
-
-                //if(_displayBox.Body.LinearVelocity.X == 0)
-                //{
-                //    MathHelper.Clamp(_displayBox.Body.LinearVelocity.Y, -_maxVelocity, _maxVelocity);
-                //}
-                //else if(_displayBox.Body.LinearVelocity.Y == 0)
-                //{
-                //    MathHelper.Clamp(_displayBox.Body.LinearVelocity.X, -_maxVelocity, _maxVelocity);
-                //}
-                //else
-                //{
-                _displayBox.Body.LinearVelocity = new Vector2(
-                    MathHelper.Clamp(_displayBox.Body.LinearVelocity.Y, -_maxAngleVelocity, _maxAngleVelocity),
-                    MathHelper.Clamp(_displayBox.Body.LinearVelocity.X, -_maxAngleVelocity, _maxAngleVelocity)
+                _physicsBody.LinearVelocity = new Vector2(
+                    MathHelper.Clamp(horiI, -_maxAngleVelocity, _maxAngleVelocity),
+                    MathHelper.Clamp(vertI, -_maxAngleVelocity, _maxAngleVelocity)
                     );
-                //}
             }
+
+            _displayRect.Position = ConvertUnits.ToDisplayUnits(_physicsBody.Position);
         }
     }
 }
